ecf9623bfae7930bc88ba3b75a5fc4a9cd5ff127
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2014-03-29 00:47:43 +0100

    wiki_plus: add missing files

--- wiki_plus/management/commands/create_wiki_folders.py
+++ wiki_plus/management/commands/create_wiki_folders.py
@@ -17,16 +17,5 @@ class Command(BaseCommand):
         # be sure there is a root
         management.call_command('sync_root')
         uvs = fileuploader_models.Upload.objects.values_list('uv').distinct()
-        root_user = auth_models.User.objects.get(pk=1)
-        root_article = wiki_models.Article.objects.get(pk=1)
-        root_urlpath = wiki_models.URLPath.objects.get(pk=1)
-        article_kwargs = {
-            'owner': root_user,
-            'group': root_article.group,
-            'group_read': root_article.group_read,
-            'group_write': root_article.group_write,
-            'other_read': root_article.other_read,
-            'other_write': False,#root_article.other_write,
-        }
         for uv in (uv[0] for uv in uvs):
             create_folders(uv)
