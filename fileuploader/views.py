e1613e19ca82b4d79a43d02f8751f6501a3570ac
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2014-03-29 10:31:51 +0100

    Fix redirect on adminlogin

--- fileuploader/views.py
+++ fileuploader/views.py
@@ -43,7 +43,7 @@ def upload(request):
 def adminlogin(request):
     """Small tweak to allow classic django login (bypassing CAS)"""
     request.GET = request.GET.copy()
-    request.GET['redirect'] = '/admin/'
+    request.GET['redirect'] = '/annaleut/admin/'
     return auth_views.login(request, template_name='admin/login.html',
                             redirect_field_name='redirect')
 
